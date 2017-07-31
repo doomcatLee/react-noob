0463996392d74af7dc9377d5cbe90acd82a9677a
Alex Lee <doomcat9167@gmail.com>
2017-07-31 14:51:02 -0700

    add event binder to constructor in NewTicketControl

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -5,9 +5,12 @@ class NewTicketControl extends React.Component{
   constructor(props){
     super(props);
     this.state = {formVisibleOnPage:false};
+    this.handleDisplayingNewTicketForm = this.handleDisplayingNewTicketForm.bind(this);
   }
 
   handleDisplayingNewTicketForm(event){
+    this.setState({formVisibleOnPage: true});
+    console.log(this.state);
     console.log('New ticket has been clicked!');
   }
 
