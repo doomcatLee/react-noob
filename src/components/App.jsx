11f9c51e9b0c7aa1560116984aa60e5f5e3d669d
Alex Lee <doomcat9167@gmail.com>
2017-07-30 19:40:38 -0700

    add header.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header'
+
+function App(props){
+  return(
+    <div>
+      <Header/>
+      <h1>Help Queue</h1>
+    </div>
+  )
+}
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import App from './components/App';
 
 ReactDOM.render(
-  <div>
-    <h1>Help Queue</h1>
-    <h2>{new Date().toLocaleTimeString()}</h2>
-  </div>,
+  <App/>,
   document.getElementById('react-app-root')
 );
